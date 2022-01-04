7d2f5c4d55805813edc4ef95e60f73978fce770e
umarhunter <umor.uddin54@myhunter.cuny.edu>
2022-01-04 15:38:10 -0500

    - added comments - added/fixed while loop - moved num variables inside each if statement to do: get rid of ==, move code into functions, etc

--- consolecalculator/main.cpp
+++ consolecalculator/main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
-enum class Values{Add,Subtract,Multiply,Divide};
+enum class Values{Add,Subtract,Multiply,Divide}; // this enum class contains values (data) that I'll need in order to run operations
 
-struct Calculator {
-    Calculator(Values init) {
+struct Calculator { //
+    Calculator(Values init) { //constructor initializes the value/operation to avoid issues
         operation = init;
     }
-    int calculate(int a, int b) {
+    int calculate(int a, int b) { // functions takes two integer inputs a and b then does computation based off the operation variable
         switch (operation) {
             case Values::Add: {
                 return a + b;
@@ -22,38 +22,62 @@ struct Calculator {
             } break;
         }
     }
-    Values operation;
+    Values operation; // initializing the object of type 'Values' named operation
 };
-int main() {
-    int num1{};
-    int num2{};
-    int while_loop{0};
-    std::string operation{};
 
-    Calculator set_to_add{Values::Add};
+int exitfunction(int a) {
+    std::cout << "\nThank you for using the calculator! Please come again :)";
+    return a = 5;
+}
+
+int main() {
+    int num1{}; // first input #
+    int num2{}; // second input #
+    int while_loop = 0; // while loop variable
+    std::string operation{}; // string variable that will take user operation input
+
+    Calculator set_to_add{Values::Add}; // creating elements in Calculator to initialize it and prepare for the computations ahead (with the variables we created)
     Calculator set_to_sub{Values::Subtract};
     Calculator set_to_mult{Values::Multiply};
     Calculator set_to_div{Values::Divide};
 
+    std::cout << "Welcome to my humble console calculator. Your choices of operation are: Add, Subtract, Multiply, and Divide.\n";
+    std::cout << "NOTE: Make sure you don't have any extra characters or spaces as this may affect your output!\n";
 
-    std::cout << "Enter operation";
-    std::cin >> operation;
-    std::cout << "Enter first number";
-    std::cin >> num1;
-    std::cout << "Enter second number";
-    std::cin >> num2;
+    while (while_loop == 0) {
+        std::cout << "Enter operation: \n";
+        std::cin >> operation;
 
-    if (operation == "Add")
-        std::cout << num1 << " + " << num2 << " = " << set_to_add.calculate(num1, num2);
-    else if (operation == "Subtract")
-        std::cout << num1 << " - " << num2 << " = " << set_to_sub.calculate(num1, num2);
-    else if (operation == "Multiply")
-        std::cout << num1 << " * " << num2 << " = " << set_to_mult.calculate(num1, num2);
-    else if (operation == "Divide")
-        std::cout << num1 << " / " << num2 << " = " << set_to_div.calculate(num1, num2);
-    else
-        while_loop = 1;
-    std::cout << "\n Thank you for using the calculator!";
+        if (operation == "Add") {
+            std::cout << "Enter first number: \n";
+            std::cin >> num1;
+            std::cout << "Enter second number: \n";
+            std::cin >> num2;
+            std::cout << num1 << " + " << num2 << " = " << set_to_add.calculate(num1, num2);
+            }
+        else if (operation == "Subtract") {
+            std::cout << "Enter first number: \n";
+            std::cin >> num1;
+            std::cout << "Enter second number: \n";
+            std::cin >> num2;
+            std::cout << num1 << " - " << num2 << " = " << set_to_sub.calculate(num1, num2); }
+        else if (operation == "Multiply") {
+            std::cout << "Enter first number: \n";
+            std::cin >> num1;
+            std::cout << "Enter second number: \n";
+            std::cin >> num2;
+            std::cout << num1 << " * " << num2 << " = " << set_to_mult.calculate(num1, num2);
+        }
+        else if (operation == "Divide") {
+            std::cout << "Enter first number: \n";
+            std::cin >> num1;
+            std::cout << "Enter second number: \n";
+            std::cin >> num2;
+            std::cout << num1 << " / " << num2 << " = " << set_to_div.calculate(num1, num2);
+        }
+        else
+            while_loop = exitfunction(while_loop); // breaks the loop
 
+    }
 
 }
